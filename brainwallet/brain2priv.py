9b316093843c56367f408eb37199e47f17afd86d
awchjimmy <awchjimmy@gmail.com>
2024-02-21 13:42:29 +0800

    Update brain2priv.py

--- brainwallet/brain2priv.py
+++ brainwallet/brain2priv.py
@@ -7,11 +7,14 @@ da2876b3eb31edb4436fa4650673fc6f01f90de2f1793c4ec332b2387b09726f
 
 import sys
 import hashlib
+import fileinput
 
-def brain2priv():
-    brain = sys.argv[1]
+def brain2priv(brain):
     priv = hashlib.sha256(brain.encode('utf-8')).hexdigest()
-    print(priv)
-    
+    return priv
+
 if __name__ == '__main__':
-    brain2priv()
+    with fileinput.input() as f:
+        for line in f:
+            priv = brain2priv(line.rstrip())
+            print(priv)
